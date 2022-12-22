1cecc93ee52d3fe5e4d9626ad73bfa4dad08c880
Gabriel <gabriel.teles.santos@hotmail.com>
2022-12-22 17:57:11 -0300

    retornando value em branco para a input area e add alert ao copy

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ function criptografar () {
     var resultDescripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
     document.getElementById('output-area').innerHTML =  '<textarea readonly id="result">' + resultDescripto + '</textarea>' + '<button id="copy" onclick="copiar()">Copiar</button>'
+
+    
+    document.getElementById('input-text').value = " ";
 }
 function descriptografar () {
     var texto = textInput.value
@@ -14,10 +17,14 @@ function descriptografar () {
     var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
     document.getElementById('output-area').innerHTML =  '<textarea readonly id="result">' + resultDescripto + '</textarea>' + '<button id="copy" onclick="copiar()">Copiar</button>'
+    
+    document.getElementById('input-text').value = " ";
 }
 function copiar() {
     var textoCop = document.getElementById('result');
   
     textoCop.select();
     document.execCommand('copy');
+    document.getElementById('result').value = " ";
+    alert('Texto copiado para a área de transferência')
 }
